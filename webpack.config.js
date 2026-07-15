284e75e66f588342e19b0333a7ed508bf06d0566
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-15 07:03:14 +0400

    chore(webpack): set publicPath back to /ci-cd/

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/ci-cd/',
     clean: true,
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/[name][ext]',
